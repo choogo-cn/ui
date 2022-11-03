babf2df322a4b15ab87464bec782574fe7e15c09
JetLu <i@lufei.im>
2022-11-03 22:38:58 +0800

    update

--- src/lib/toast/toast.svelte
+++ src/lib/toast/toast.svelte
@@ -1,34 +1,50 @@
 <script lang="ts">
-  // todo
-  // export let position: {
-  //   x: 'center' | 'left' | 'right'
-  //   y: 'top' | 'bottom'
-  // } = {
-  //   x: 'center',
-  //   y: 'top'
-  // }
+  import {css} from '@emotion/css'
+  import {cls} from '$lib/util'
+    import deepmerge from 'deepmerge';
+
+  export let autoClose = true
+
+  const _position = {x: 'center', y: 'top'} as typeof position
+
+  export let position: {
+    x?: 'center' | 'left' | 'right'
+    y?: 'top' | 'bottom' | 'center'
+  } = {
+    x: 'center',
+    y: 'top'
+  }
+
+  $: $config.autoClose = autoClose
+  $: position = deepmerge(_position, position)
+  $: cssID = css`${[
+    position.x === 'left' ? 'left: 1rem' : position.x === 'right' ? 'right: 1rem' : 'left: 0; right: 0',
+    position.y === 'top' ? 'top: 1rem' : position.y === 'bottom' ? 'bottom: 1rem' : 'top: 0; bottom: 0'
+  ].join(';')}`
 </script>
 
 <script lang="ts" context="module">
   import {writable, get} from 'svelte/store'
   import {slide} from 'svelte/transition'
   const list = writable<{id: number, msg: string}[]>([])
+  const config = writable({autoClose: true})
   let id = 0
+
   export function toast(msg: string, opts: {duration: number} = {duration: 3}) {
     const _id = id++
 
     list.set([...get(list), {msg, id: _id}])
 
-    setTimeout(() => {
+    get(config).autoClose && setTimeout(() => {
       list.set(get(list).filter(item => item.id !== _id))
     }, opts.duration * 1e3)
   }
 </script>
 
-<section class="w-fit">
+<section class={cls(cssID)}>
   {#each $list as {msg}}
     <div class="text-center text-sm"
-      transition:slide={{delay: 0, duration: 3e2}}
+      transition:slide|local={{delay: 0, duration: 3e2}}
     >{msg}</div>
   {/each}
 </section>
@@ -37,10 +53,8 @@
 <style lang="scss">
   section {
     position: fixed;
-    left: 0;
-    right: 0;
-    top: 1rem;
     margin: auto;
+    height: fit-content;
     display: flex;
     flex-direction: column;
     gap: .5rem;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,8 +14,6 @@
   <Button onClick={() => dialog.visible = true}>open dialog</Button>
   <Button onClick={() => toast(`count: ${count++}`)}>open toast</Button>
   <Button onClick={() => toast('ok')} variant="text"><Account color="warning"/></Button>
-  <Dialog open={dialog.visible} onClose={() => dialog.visible = false}>
-    ok
-  </Dialog>
+  <Dialog open={dialog.visible} onClose={() => dialog.visible = false}>ok</Dialog>
   <Toast/>
 <!-- </ThemeProvider> -->
