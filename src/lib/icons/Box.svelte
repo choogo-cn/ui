5c749d4c028794fb26d0d005503b024f236ad8e9
JetLu <i@lufei.im>
2022-11-11 23:27:52 +0800

    update

---
+++ src/lib/icons/Box.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import {getContext} from 'svelte'
+
+  let cls = null as string
+  export {cls as class}
+
+  export let style = ''
+  export let color: keyof Theme = 'primary'
+  let theme = getContext<Theme>('theme') ?? defaultTheme
+  console.log(color)
+</script>
+
+<script lang="ts" context="module">
+  import {defaultTheme} from '$lib/components/ThemeProvider.svelte'
+</script>
+
+<svg
+  {style}
+  class={cls}
+  focusable="false"
+  aria-hidden="true"
+  viewBox="0 0 24 24"
+  fill={theme[color].contrastText}
+><slot/></svg>
+
+<style lang="scss">
+  @use "./styles/core";
+
+  svg {
+    @extend %svg;
+  }
+</style>
